60e8633dc396148a9a4766afb7e631b94bcce9e7
fizici1 <fizikchim@gmail.com>
2019-03-20 18:59:32 +0400

    Gestion des élèves

--- src/Controller/EleveController.php
+++ src/Controller/EleveController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 use App\Entity\Eleve;
 use App\Repository\EleveRepository;
+use App\Form\EleveType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class EleveController extends AbstractController
 {
@@ -20,12 +23,48 @@ class EleveController extends AbstractController
       ]);
     }
 
-    public function show(EleveRepository $repository, Eleve $eleve): Response
+    public function show(Eleve $eleve, Request $request, ObjectManager $em): Response
     {
+      $form = $this->createForm(EleveType::class, $eleve);
+      $form->handleRequest($request);
+
+      if ($form->isSubmitted() && $form->isValid()) {
+        $em->flush();
+      }
+
       return $this->render('eleve/show.html.twig', [
         'current_menu' => 'eleves',
-        'eleve'        => $eleve
+        'eleve'        => $eleve,
+        'form'         => $form->createView()
       ]);
     }
+
+    public function new(Request $request, ObjectManager $em)
+    {
+      $eleve = new Eleve();
+      $form = $this->createForm(EleveType::class, $eleve);
+      $form->handleRequest($request);
+
+      if ($form->isSubmitted() && $form->isValid()) {
+        $em->persist($eleve);
+        $em->flush();
+        return $this->redirectToRoute('eleves');
+      }
+
+      return $this->render('eleve/new.html.twig', [
+        'current_menu' => 'eleves',
+        'eleve'        => $eleve,
+        'form'         => $form->createView()
+      ]);
+    }
+
+    public function delete(Eleve $eleve, Request $request, ObjectManager $em)
+    {
+      if ($this->isCsrfTokenValid('delete' . $eleve->getId(), $request->get('_token'))) {
+            $em->remove($eleve);
+            $em->flush();
+        }
+        return $this->redirectToRoute('eleves');
+      }
 }
  ?>
---
+++ src/Form/EleveType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Eleve;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EleveType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('login')
+            ->add('password')
+            ->add('classe')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Eleve::class,
+        ]);
+    }
+}
